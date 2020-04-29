4246d125947a81e4ca75d5092d4a08bf0aaded2e
Noah Alvarado <np3474@gmail.com>
2020-04-29 00:25:39 -0500

    h2 testing

--- cat-c/src/CatPass.cpp
+++ cat-c/src/CatPass.cpp
@@ -111,12 +111,12 @@ namespace {
                 if (isa<CallInst>(result)) kill_sets[&I].insert(cast<CallInst>(result));
 
                 for (auto user : result->users()) {
-                  errs() << *user << "\n";
                   if (isa<CallInst>(user)) {
                     CallInst *ci = cast<CallInst>(user);
                     switch (funcToCatCode(ci->getName())) {
                       case cat_add:
-                        errs() << *ci << "\n";
+                        ci->print(errs());
+                        errs() << "\n";
                         break;
                       case cat_sub:
                         errs() << *ci << "\n";
